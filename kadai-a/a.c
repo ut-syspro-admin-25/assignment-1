f8eb8e82361dee5c8442eecb5f92a47273fff6ff
momo-trip <73386097+momo-trip@users.noreply.github.com>
2024-04-08 06:36:35 +0900

    Update a.c

--- kadai-a/a.c
+++ kadai-a/a.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include "c.h"
 
 const char *a = "Hello, C++ world!\n";
@@ -7,8 +6,8 @@ const char *a = "Hello, C++ world!\n";
 int main(void)
 {
 	const char *s = hello_c();
-	if (strcmp(s, a) < 0)
-		printf("%s", s);
+	if (mystrcmp(s, a) < 0)
+		printf(s);
 	else
-		printf("%s", a);
+		printf(a);
 }
